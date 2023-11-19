afc8cf9bd0aa73626757bfbf810902d7f1ef4e41
Artur Petrov <artur.petrov@auto.ria.com>
2023-11-19 03:51:28 +0200

    Update

--- src/guards/authGuard.js
+++ src/guards/authGuard.js
@@ -1,10 +1,10 @@
 import store from '../store';
 
 export default async function (to, from, next) {
-  await store.dispatch('user/me');
-  if (!store.state.user?.login) {
-    this.$router.push({ name: 'login' });
-  } else {
+  await store.dispatch('user/me')
+    .catch(() => this.$router.push({ name: 'login' }));
+
+  if (store.state.user?.login) {
     next();
   }
 }
